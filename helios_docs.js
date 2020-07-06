5af42b4ad831563730efd755f88144e80a64157f
FinnWalker <finn3walker@gmail.com>
2020-07-06 15:12:41 +0100

    test

--- helios_docs.js
+++ helios_docs.js
@@ -3,8 +3,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.use("/documentation/demo", express.static("demo"));
 app.use("/documentation", express.static("documentation"));
-app.use("/demo", express.static("demo"));
 
 const port = 3000;
 app.listen(port, "0.0.0.0", () => {
